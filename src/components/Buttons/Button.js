198a0d65dc41bd7067e68ecf9dd34fa359405d6b
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-19 14:13:40 +0530

    Updated

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,17 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
-import Entypo from 'react-native-vector-icons/Entypo'
+import LinearGradient from 'react-native-linear-gradient';
 function Button(props) {
     // console.warn(props.data)
     return (
-        <View style={{alignItems: 'center',marginBottom:10 }}>
-            <TouchableOpacity style={{borderWidth:1, width: '80%', height:50, borderRadius:30, justifyContent: 'center', alignItems: 'center', }}>
-                <Text style={{fontWeight: 'bold', fontSize:19}}>LOGIN</Text>
+        <View style={{alignItems: 'center',marginBottom:10, }}>
+            <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={[ '#3827b2','#6c18a4',]} style={{width: '80%',borderRadius:30}} >
+            <TouchableOpacity style={{ height:50, borderRadius:30, justifyContent: 'center', alignItems: 'center', }}>
+            
+            <Text style={{fontWeight: 'bold', fontSize:19, color: 'white'}}>LOGIN</Text>
+              
             </TouchableOpacity>
+            </LinearGradient>  
         </View>
     );
 }
 
 export default Button;  
 
+const styles = StyleSheet.create({
+    linearGradient: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5,
+        height: 10
+    },
+    buttonText: {
+        fontSize: 18,
+        fontFamily: 'Gill Sans',
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
+});
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,7 +13,7 @@ function Login() {
     return (
         <View style={{ flex: 1, }}>
             <View style={{flex:5.3}}>
-                <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={[ '#6548be','#4a23ae',]} style={styles.linearGradient}>
+                <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={[ '#4a21b2','#6519a0',]} style={styles.linearGradient}>
                     {/* <Text style={styles.buttonText}>Sign in with Facebook</Text> */}
                 </LinearGradient>
             </View>
@@ -24,7 +24,7 @@ function Login() {
                     <Text style={styles.buttonText}>Sign in with Facebook</Text>
                 </LinearGradient> */}
             </View>
-            <View style={{backgroundColor: 'white', position: 'absolute', top:Height/5, width: '95%', alignSelf: 'center', height:Height/1.82, borderRadius:20, elevation:5}}>
+            <View style={{backgroundColor: 'white', position: 'absolute', top:Height/5, width: '90%', alignSelf: 'center', height:Height/1.82, borderRadius:20, elevation:5}}>
                 <Text style={{color: 'black', alignSelf: 'center', fontWeight: 'bold', fontSize:25, padding:10}}>Sign In</Text>
                 <Input data={'Enter Your Email'} icon={Entypo} name={"mail"}/>
                 <Input data={'Password'} icon={Entypo} name={"key"}/>
